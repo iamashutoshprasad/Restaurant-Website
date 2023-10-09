5d02a1556d2e220aacdeb1cb7783912670f534fd
Ashu <ashuprasad1227@gmail.com>
2023-10-09 21:40:18 +0530

    cart functionality added with backdrop and close button working

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -2,11 +2,14 @@ import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
 const HeaderCartButton = (props) => {
-    return <button className={classes.button}>
-        <span className={classes.icon}><CartIcon /></span>
-
-        <span>Cart</span>
+    return (<button className={classes.button} onClick={props.onClick}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span> Your Cart</span>
         <span className={classes.badge}> 3</span>
     </button>
+    )
 }
 export default HeaderCartButton
+
